f8965628c753ce082758fb7bf99eed4ae9ca9d90
razvan tudorica <razvan.tudorica@picsart.com>
2024-11-25 15:25:13 +0100

    upscale e2e, parametrize

--- tests/e2e/test_upload.py
+++ tests/e2e/test_upload.py
@@ -11,12 +11,25 @@ from picsart_sdk.clients.upload_client import UploadClient
     not os.getenv("PICSART_API_KEY"),
     reason="PICSART_API_KEY environment variable is not set",
 )
-def test_upload_from_file():
+@pytest.mark.parametrize(
+    "method, param",
+    [
+        ("upload_from_path", "../resources/image1.jpeg"),
+        (
+            "upload_from_url",
+            "https://pastatic.picsart.com/cms-pastatic/49a41b68-a0c9-42c0-aed5-58296b4c5379.jpeg",
+        ),
+    ],
+)
+def test_upload(method, param):
     client: UploadClient = picsart_sdk.client("upload")
-    current_dir = os.path.dirname(__file__)
-    file_path = os.path.join(current_dir, "../resources/image1.jpeg")
 
-    result = client.upload_from_path(file_path=file_path)
+    if method == "upload_from_path":
+        current_dir = os.path.dirname(__file__)
+        param = os.path.abspath(os.path.join(current_dir, param))
+
+    function = getattr(client, method)
+    result = function(param)
 
     assert isinstance(result, ApiResponse)
     assert isinstance(result.data, ApiResponseData)
