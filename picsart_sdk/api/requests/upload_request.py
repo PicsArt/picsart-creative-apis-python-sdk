6159b3247221a7bad4d9fe637b5189ffdebbf559
razvan tudorica <razvan.tudorica@picsart.com>
2024-11-25 13:53:58 +0100

    remove useless files; unittests

--- picsart_sdk/api/requests/upload_request.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from typing import Optional, IO
-
-
-@dataclass
-class UploadRequest:
-    file_path: Optional[str]
-    file_url: Optional[str]
-    file_descriptor: Optional[IO[bytes]]
--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -1,23 +1,27 @@
-import unittest
+from pytest import fixture
 
 import picsart_sdk
 
+@fixture
+def client_name_class_map():
+    return {
+        "upload": "UploadClient",
+        "removebg": "RemoveBackgroundClient",
+        "upscale": "UpscaleClient"
+    }
 
-class TestClient(unittest.TestCase):
-    def setUp(self):
-        self.client_name_class_map = {
-            "upload": "UploadClient",
-            "removebg": "RemoveBackgroundClient",
-            "upscale": "UpscaleClient"
-        }
 
-    def test_client_type(self):
-        for client_name, expected_class_name in self.client_name_class_map.items():
-            client = picsart_sdk.client(client_name)
-            self.assertEqual(client.__class__.__name__, expected_class_name)
+def test_client_type(client_name_class_map):
+    for client_name, expected_class_name in client_name_class_map.items():
+        client = picsart_sdk.client(client_name)
+        assert client.__class__.__name__ == expected_class_name, (
+            f"Expected {expected_class_name}, but got {client.__class__.__name__} for client '{client_name}'"
+        )
 
-    def test_client_from_session(self):
-        session = picsart_sdk.Session(api_key="API_KEY")
-        for client_name, expected_class_name in self.client_name_class_map.items():
-            client = session.client(client_name)
-            self.assertEqual(client.__class__.__name__, expected_class_name)
+def test_client_from_session(client_name_class_map):
+    session = picsart_sdk.Session(api_key="API_KEY")
+    for client_name, expected_class_name in client_name_class_map.items():
+        client = session.client(client_name)
+        assert client.__class__.__name__ == expected_class_name, (
+            f"Expected {expected_class_name}, but got {client.__class__.__name__} for client '{client_name}'"
+        )
