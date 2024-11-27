7e39a110fe882d899137afd1a3af70eb140c2a1b
razvan tudorica <razvan.tudorica@picsart.com>
2024-11-27 11:04:32 +0100

    remove all code; moved to another branch for MR

--- picsart_sdk/clients/requests_models/upload_request.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-from picsart_sdk.clients.base.base_request import BaseRequest
-from picsart_sdk.clients.requests_models.picsart_image import PicsartImage
-
-
-@dataclass
-class UploadRequest(BaseRequest):
-    image: PicsartImage
--- tests/e2e/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-The tests from this folder need a valid `PICSART_API_KEY` environment variable to be set.
-"""
