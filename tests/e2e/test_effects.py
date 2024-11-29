00289d7b658c821ce3af9ba835275408db7bf1bf
razvan tudorica <razvan.tudorica@picsart.com>
2024-11-29 22:43:21 +0100

    fix tests

--- tests/e2e/test_effects.py
+++ tests/e2e/test_effects.py
@@ -1,7 +1,12 @@
 import picsart_sdk
+from picsart_sdk.api_responses.effects_responses import EffectsList
 from picsart_sdk.clients import EffectsClient
 
 
 def test_get_effects():
     client: EffectsClient = picsart_sdk.client("effects")
-    client.get_effects()
+    result = client.get_available_effects()
+
+    assert isinstance(result, EffectsList)
+    assert isinstance(result.effects, list)
+    assert len(result.effects) >= 1
--- tests/e2e/test_generic_async.py
+++ tests/e2e/test_generic_async.py
@@ -12,54 +12,85 @@ from picsart_sdk.api_responses import ApiResponse, ApiResponseData
 )
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
-    "client_name, method_name, image_path, image_url",
+    "client_name, method_name, image_path, image_url, extra_params",
     [
-        ("remove_background", "remove_background", "../resources/image1.jpeg", None),
+        (
+            "remove_background",
+            "remove_background",
+            "../resources/image1.jpeg",
+            None,
+            None,
+        ),
         (
             "remove_background",
             "remove_background",
             None,
             "https://pastatic.picsart.com/cms-pastatic/49a41b68-a0c9-42c0-aed5-58296b4c5379.jpeg",
+            None,
         ),
-        ("upscale", "upscale", "../resources/image1.jpeg", None),
+        ("upscale", "upscale", "../resources/image1.jpeg", None, None, ),
         (
             "upscale",
             "upscale",
             None,
             "https://pastatic.picsart.com/cms-pastatic/49a41b68-a0c9-42c0-aed5-58296b4c5379.jpeg",
+            None,
         ),
-        ("ultra_upscale", "ultra_upscale", "../resources/image1.jpeg", None),
+        ("ultra_upscale", "ultra_upscale", "../resources/image1.jpeg", None, None, ),
         (
             "ultra_upscale",
             "ultra_upscale",
             None,
             "https://pastatic.picsart.com/cms-pastatic/49a41b68-a0c9-42c0-aed5-58296b4c5379.jpeg",
+            None,
         ),
-        ("ultra_enhance", "ultra_enhance", "../resources/image1.jpeg", None),
+        ("ultra_enhance", "ultra_enhance", "../resources/image1.jpeg", None, None, ),
         (
             "ultra_enhance",
             "ultra_enhance",
             None,
             "https://pastatic.picsart.com/cms-pastatic/49a41b68-a0c9-42c0-aed5-58296b4c5379.jpeg",
+            None,
         ),
-        ("face_enhancement", "face_enhancement", "../resources/image1.jpeg", None),
+        ("face_enhancement", "face_enhancement", "../resources/image1.jpeg", None, None, ),
         (
             "face_enhancement",
             "face_enhancement",
             None,
             "https://pastatic.picsart.com/cms-pastatic/49a41b68-a0c9-42c0-aed5-58296b4c5379.jpeg",
+            None,
         ),
-        ("upload", "upload_image", "../resources/image1.jpeg", None),
+        ("upload", "upload_image", "../resources/image1.jpeg", None, None, ),
         (
             "upload",
             "upload_image",
             None,
             "https://pastatic.picsart.com/cms-pastatic/49a41b68-a0c9-42c0-aed5-58296b4c5379.jpeg",
+            None,
+        ),
+        (
+            "effects",
+            "effects",
+            "../resources/image1.jpeg",
+            None,
+            {"effect_name": "apr1"},
+        ),
+        (
+            "effects",
+            "effects",
+            None,
+            "https://pastatic.picsart.com/cms-pastatic/49a41b68-a0c9-42c0-aed5-58296b4c5379.jpeg",
+            {"effect_name": "apr1"},
         ),
     ],
 )
-async def test_generic_async(client_name, method_name, image_path, image_url):
-    client = picsart_sdk.async_client(client_name)
+async def test_generic_async(
+    client_name, method_name, image_path, image_url, extra_params
+):
+    if extra_params is None:
+        extra_params = {}
+
+    client = picsart_sdk.client(client_name)
 
     params = {"image_url": image_url}
     if image_path:
@@ -67,7 +98,7 @@ async def test_generic_async(client_name, method_name, image_path, image_url):
         params = {"image_path": os.path.abspath(os.path.join(current_dir, image_path))}
 
     function = getattr(client, method_name)
-    result = await function(**params)
+    result = function(**{**params, **extra_params})
 
     assert isinstance(result, ApiResponse)
     assert isinstance(result.data, ApiResponseData)
