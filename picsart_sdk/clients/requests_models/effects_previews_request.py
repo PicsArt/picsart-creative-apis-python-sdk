d1343140b98277ff26c6ef0d030436a3084dee49
razvan tudorica <razvan.tudorica@picsart.com>
2024-12-04 00:43:28 +0100

    fix import; add async unittests

--- picsart_sdk/clients/requests_models/effects_previews_request.py
+++ picsart_sdk/clients/requests_models/effects_previews_request.py
@@ -2,7 +2,10 @@ from dataclasses import dataclass
 from typing import Optional
 
 from picsart_sdk.clients.base.base_request import BaseRequest
-from picsart_sdk.clients.requests_models import PicsartImage, PicsartImageFormat
+from picsart_sdk.clients.requests_models.picsart_image import (
+    PicsartImage,
+    PicsartImageFormat,
+)
 
 
 @dataclass
