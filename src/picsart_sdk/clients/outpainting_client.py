0956e73f67b5f20320c6ca82a1e47e8cda594c4e
razvan tudorica <razvan.tudorica@picsart.com>
2024-12-12 12:03:08 +0100

    separate in and out painting

---
+++ src/picsart_sdk/clients/outpainting_client.py
@@ -0,0 +1,65 @@
+from typing import Optional, Union
+
+from picsart_sdk.api_responses.painting_response import PaintingApiResponse
+from picsart_sdk.clients.common_painting_client import OutpaintingCommon
+from picsart_sdk.clients.requests_models import PicsartImageFormat
+from picsart_sdk.clients.requests_models.painting_request import PaintingMode
+
+
+class OutpaintingClient(OutpaintingCommon):
+
+    def get_result(self, inference_id: str) -> PaintingApiResponse:
+        return self.get(postfix_url=inference_id)
+
+    def outpainting(
+        self,
+        prompt: str,
+        image_url: Optional[str] = None,
+        image_path: Optional[str] = None,
+        mask_url: Optional[str] = None,
+        mask_path: Optional[str] = None,
+        negative_prompt: Optional[str] = None,
+        count: Optional[int] = 4,
+        output_format: Optional[PicsartImageFormat] = PicsartImageFormat.PNG,
+        mode: Optional[PaintingMode] = PaintingMode.SYNC,
+    ) -> PaintingApiResponse:
+        return super().sync_inpainting_request(
+            prompt=prompt,
+            image_url=image_url,
+            image_path=image_path,
+            mask_url=mask_url,
+            mask_path=mask_path,
+            negative_prompt=negative_prompt,
+            count=count,
+            output_format=output_format,
+            mode=mode,
+        )
+
+
+class AsyncOutpaintingClient(OutpaintingCommon):
+    async def get_result(self, inference_id: str) -> PaintingApiResponse:
+        return await self.async_get(postfix_url=inference_id)
+
+    async def outpainting(
+        self,
+        prompt: str,
+        image_url: Optional[str] = None,
+        image_path: Optional[str] = None,
+        mask_url: Optional[str] = None,
+        mask_path: Optional[str] = None,
+        negative_prompt: Optional[str] = None,
+        count: Optional[int] = 4,
+        output_format: Optional[PicsartImageFormat] = PicsartImageFormat.PNG,
+        mode: Optional[PaintingMode] = PaintingMode.SYNC,
+    ) -> PaintingApiResponse:
+        return await super().async_inpainting_request(
+            prompt=prompt,
+            image_url=image_url,
+            image_path=image_path,
+            mask_url=mask_url,
+            mask_path=mask_path,
+            negative_prompt=negative_prompt,
+            count=count,
+            output_format=output_format,
+            mode=mode,
+        )
