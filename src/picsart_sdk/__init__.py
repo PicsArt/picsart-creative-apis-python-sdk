c5cc0979601c52a10c8d408379daf59431d2374d
razvan tudorica <razvan.tudorica@picsart.com>
2024-12-13 15:28:09 +0100

    allow api key to be passed at the client creation

--- src/picsart_sdk/__init__.py
+++ src/picsart_sdk/__init__.py
@@ -2,21 +2,26 @@ from picsart_sdk.clients.http_clients import AsyncHttpClient, HttpClient
 from picsart_sdk.core.session import Session
 
 
-def client(client_name: str, is_async: bool = False, *args, **kwargs):
+def client(client_name: str, is_async: bool = False, api_key=None, *args, **kwargs):
     """
-    Create a low-level service client by name using the default session.
+    Create a low-level service client by name.
 
-    See :py:meth:`boto3.session.Session.client`.
+    Returns an implementation of BaseClient class (eg: RemoveBackgroundClient).
     """
-    return Session.get_default_session().client(
-        client_name=client_name, is_async=is_async, *args, **kwargs
+    if api_key:
+        session = Session(api_key=api_key)
+    else:
+        session = Session.get_default_session()
+
+    return session.client(client_name=client_name, is_async=is_async, *args, **kwargs)
+
+
+def async_client(client_name: str, api_key=None, *args, **kwargs):
+    """
+    Create a low-level async service client by name.
+
+    Returns an async implementation of BaseClient class (eg: AsyncRemoveBackgroundClient).
+    """
+    return client(
+        client_name=client_name, is_async=True, api_key=api_key, *args, **kwargs
     )
-
-
-def async_client(client_name: str, *args, **kwargs):
-    """
-    Create a low-level async service client by name using the default session.
-
-    See :py:meth:`boto3.session.Session.client`.
-    """
-    return client(client_name=client_name, is_async=True, *args, **kwargs)
