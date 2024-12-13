97ed740d02a0092f59d84ba01a39fbd47dab6c0a
razvan tudorica <razvan.tudorica@picsart.com>
2024-12-13 15:35:28 +0100

    fix tests

--- tests/e2e/test_ai_effects.py
+++ tests/e2e/test_ai_effects.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 import picsart_sdk
@@ -5,6 +7,10 @@ from picsart_sdk.api_responses import EffectsListApiResponse
 from picsart_sdk.clients import AiEffectsClient, AsyncAiEffectsClient
 
 
+@pytest.mark.skipif(
+    not os.getenv("PICSART_API_KEY"),
+    reason="PICSART_API_KEY environment variable is not set",
+)
 def test_get_ai_effects():
     client: AiEffectsClient = picsart_sdk.client("ai_effects")
     result = client.get_available_ai_effects()
@@ -14,6 +20,10 @@ def test_get_ai_effects():
     assert len(result.effects) >= 1
 
 
+@pytest.mark.skipif(
+    not os.getenv("PICSART_API_KEY"),
+    reason="PICSART_API_KEY environment variable is not set",
+)
 @pytest.mark.asyncio
 async def test_get_effects_async():
     client: AsyncAiEffectsClient = picsart_sdk.async_client("ai_effects")
